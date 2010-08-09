9f01a02690ce0d51697c9f1bc04beb697e23df81
ealbino.bnu <ealbino.bnu@ab0bad56-5eab-acb3-826f-623702b7ea8a>
2010-08-09 01:27:21 +0000

    A reserva foi atualizada para o correçao solicitada pelo Adilson no UC4, testei um pouco, chegou a gravar a reserva, mas precisa ser testado mais para garantir que esta funcionado.

---
+++ Agendamento/src/controle/InstalacaoUC.java
@@ -0,0 +1,91 @@
+package controle;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIData;
+
+import modelo.Reserva;
+import util.JPAUtil;
+import dao.JPAReservaDao;
+
+@SessionScoped
+@ManagedBean(name="instalacaoUC")
+public class InstalacaoUC {
+	
+	private Reserva reserva = new Reserva();
+    private UIData select;
+    private boolean mostrarTabela = false;
+    private List<Reserva> reservasEmAberto = null;
+    
+    public InstalacaoUC() {
+    }
+   
+    public UIData getSelect() {
+        return select;
+    }
+
+    public void setSelect(UIData select) {
+        this.select = select;
+    }
+
+    public Reserva getReserva(){
+        return reserva;
+    }
+    
+    public String reservas() throws Exception{
+    	JPAUtil jpa = JPAUtil.getInstance();
+    	try {
+    		JPAReservaDao daoReserva = new JPAReservaDao(jpa);
+            reservasEmAberto = daoReserva.listarReservasEmAberto(reserva);
+            for(Reserva reservaAux: reservasEmAberto){
+            	reservaAux.setProjetoresDisponiveis(daoReserva.listaProjetoresDisponiveis(reservaAux));
+            }
+    		mostrarTabela = true;	
+		} finally {
+			JPAUtil.finalizar();
+		}
+		return "listarInstalacao";
+    }
+    
+    public String novaInstalacao(){
+    	reserva = new Reserva();
+    	return "formInstalacao";
+    }
+
+    
+    public boolean isMostrar() {
+    	if (mostrarTabela) { 
+    		return true; 
+    	} else { 
+    		return false; 
+    	}
+    }
+
+	public List<Reserva> getReservasEmAberto() {
+		return reservasEmAberto;
+	}
+	
+	public String salvarInstalacao() throws Exception{
+		JPAUtil jpa = JPAUtil.getInstance();
+    	try {
+    		JPAReservaDao daoReserva = new JPAReservaDao(jpa);
+    		for(Reserva reservaAux: reservasEmAberto){
+    			if(reservaAux.getProjetor() != null){
+	    			reservaAux.setInstalado(true);
+	    			daoReserva.gravar(reserva);
+    			}
+    		}
+    		/*if(daoReserva.projetoresReservados(reserva) < daoReserva.projetoresPossiveisReserva(reserva)){
+	    		daoReserva.gravar(reserva);
+	        	return "reservaSucesso";
+    		}else
+    			FacesContext.getCurrentInstance().addMessage("data", new FacesMessage("Não existem projetores disponíveis nesta data"));
+    			return null;*/
+		} finally {
+			JPAUtil.finalizar();
+		}
+		return null;
+	}
+}
--- Agendamento/src/controle/ReservaUC.java
+++ Agendamento/src/controle/ReservaUC.java
@@ -6,11 +6,9 @@ import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.component.UIComponent;
 import javax.faces.component.UIData;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
-import javax.faces.validator.ValidatorException;
 
 import modelo.Reserva;
 import modelo.Sala;
@@ -18,24 +16,29 @@ import util.JPAUtil;
 import dao.JPAReservaDao;
 import dao.JPASalaDAO;
 
-@SessionScoped
 @ManagedBean(name="reservaUC")
+@SessionScoped
 public class ReservaUC {
-	
-	private Reserva reserva = new Reserva();
-    private UIData select;
-    private final ArrayList<Integer> horarios = new ArrayList<Integer>();
-    private boolean mostrarTabela = false;
-    private List<Reserva> reservasEmAberto = null;
+
+	private UIData select;
+	private final ArrayList<Integer> horarios = new ArrayList<Integer>();
+    private Reserva reserva = new Reserva();
+    private JPASalaDAO dao;
     private List<String> blocosPeloCampus = null;
     private List<Sala> salasPeloBloco = null;
-    
-    public ReservaUC() {
-    	for(int i= 1; i <= 15; i++)
-    		horarios.add(i);
-    }
-   
-    public UIData getSelect() {
+    private Sala dadosConfiguracao = new Sala();
+
+	public ReservaUC() {
+		try {
+			dao = new JPASalaDAO(JPAUtil.getInstance());
+			for(int i= 1; i <= 15; i++)
+	    		horarios.add(i);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public UIData getSelect() {
         return select;
     }
 
@@ -43,13 +46,60 @@ public class ReservaUC {
         this.select = select;
     }
 
-    public Reserva getReserva(){
-        return reserva;
-    }
-    
-    public List<Integer> getHorarios(){
+	public Reserva getReserva() {
+		return reserva;
+	}
+
+	public void setReserva(Reserva modelo) {
+		this.reserva = modelo;
+	}
+
+	public List<Integer> getHorarios(){
     	return horarios;
     }
+	public List<Sala> getSalasPeloBloco(){
+    	return salasPeloBloco;
+    }
+    
+    public List<String> getBlocosPeloCampus(){
+    	return blocosPeloCampus;
+    }
+        
+    public void setDadosConfiguracao(Sala dadosConfiguracao) {
+		this.dadosConfiguracao = dadosConfiguracao;
+	}
+
+	public Sala getDadosConfiguracao() {
+		return dadosConfiguracao;
+	}
+    
+	public List<Sala> getSalas() {
+		List<Sala> l;
+		try {
+			l = dao.listarTodos();
+		} catch (Exception e) {
+			l = new ArrayList<Sala>();
+			e.printStackTrace();
+		}
+		return l;
+	}
+	
+	public void atualizouCampus(ValueChangeEvent event) throws Exception{
+		dadosConfiguracao.setCampus(Integer.valueOf(String.valueOf(event.getNewValue())));
+    	atualizarDadosCombo();
+    }
+	
+	public void atualizarSalasPeloBloco(ValueChangeEvent event) throws Exception{
+		dadosConfiguracao.setBloco((String)event.getNewValue());
+    	salasPeloBloco = dao.listaSalasPeloBlocoECampus(dadosConfiguracao);            	
+    }
+    
+    private void atualizarDadosCombo()throws Exception{
+    	blocosPeloCampus = dao.listaBlocosPeloCampus(dadosConfiguracao);
+		if(blocosPeloCampus.size() > 0)//atribuir o valor do primeiro bloco para vir a pesquisa com as salas
+			dadosConfiguracao.setBloco(blocosPeloCampus.get(0));
+		salasPeloBloco = dao.listaSalasPeloBlocoECampus(dadosConfiguracao);  
+   }
     
     public String salvar() throws Exception{
     	JPAUtil jpa = JPAUtil.getInstance();
@@ -67,11 +117,10 @@ public class ReservaUC {
 		}
     }
     
-    
-
     public String novo() throws Exception{
         reserva = new Reserva();
-        atualizarDadosCombo(reserva.getSala());
+        dadosConfiguracao = new Sala();
+        atualizarDadosCombo();
         return "formReserva";
     }
 
@@ -79,101 +128,6 @@ public class ReservaUC {
          return "indexAdmin";
     }
     
-    public List<String> getBlocosPeloCampus(){
-    	return blocosPeloCampus;
-    }
-    
-    public void atualizarSalasPeloBloco(ValueChangeEvent event) throws Exception{
-    	JPAUtil jpa = JPAUtil.getInstance();
-    	try {
-    		reserva.getSala().setBloco((String)event.getNewValue());
-    		//reserva.setSala(new JPASalaDAO(jpa).ler(1L));
-    		salasPeloBloco = new JPASalaDAO(jpa).listaSalasPeloBlocoECampus(reserva.getSala());            	
-    	} finally {
-			JPAUtil.finalizar();
-		}		
-    }
-    
-    public void atualizouCampus(ValueChangeEvent event) throws Exception{
-    	reserva.getSala().setCampus(Integer.valueOf(String.valueOf(event.getNewValue())));
-    	atualizarDadosCombo(reserva.getSala());
-    }
-    
-    public void teste(FacesContext facesContex, UIComponent validate, Object value) throws ValidatorException {
-	    reserva.setSala((Sala)value);
-	}
-    
-    private void atualizarDadosCombo(Sala sala)throws Exception{
-    	JPAUtil jpa = JPAUtil.getInstance();
-    	try {
-    		JPASalaDAO jpaSala = new JPASalaDAO(jpa);
-    		blocosPeloCampus = jpaSala.listaBlocosPeloCampus(sala);
-    		if(blocosPeloCampus.size() > 0)//atribuir o valor do primeiro bloco para vir a pesquisa com as salas
-    			sala.setBloco(blocosPeloCampus.get(0));
-//    		salasPeloBloco = jpaSala.listaSalasPeloBlocoECampus(sala);  
-    		salasPeloBloco = jpaSala.listarTodos();  
-		} finally {
-			JPAUtil.finalizar();
-		}
-    }
-    
-    public List<Sala> getSalasPeloBloco(){
-    	return salasPeloBloco;
-    }
 
-    
-    public String reservas() throws Exception{
-    	JPAUtil jpa = JPAUtil.getInstance();
-    	try {
-    		JPAReservaDao daoReserva = new JPAReservaDao(jpa);
-            reservasEmAberto = daoReserva.listarReservasEmAberto(reserva);
-            for(Reserva reservaAux: reservasEmAberto){
-            	reservaAux.setProjetoresDisponiveis(daoReserva.listaProjetoresDisponiveis(reservaAux));
-            }
-    		mostrarTabela = true;	
-		} finally {
-			JPAUtil.finalizar();
-		}
-		return "listarInstalacao";
-    }
-    
-    public String novaInstalacao(){
-    	reserva = new Reserva();
-    	return "formInstalacao";
-    }
 
-    
-    public boolean isMostrar() {
-    	if (mostrarTabela) { 
-    		return true; 
-    	} else { 
-    		return false; 
-    	}
-    }
-
-	public List<Reserva> getReservasEmAberto() {
-		return reservasEmAberto;
-	}
-	
-	public String salvarInstalacao() throws Exception{
-		JPAUtil jpa = JPAUtil.getInstance();
-    	try {
-    		JPAReservaDao daoReserva = new JPAReservaDao(jpa);
-    		for(Reserva reservaAux: reservasEmAberto){
-    			if(reservaAux.getProjetor() != null){
-	    			reservaAux.setInstalado(true);
-	    			daoReserva.gravar(reserva);
-    			}
-    		}
-    		/*if(daoReserva.projetoresReservados(reserva) < daoReserva.projetoresPossiveisReserva(reserva)){
-	    		daoReserva.gravar(reserva);
-	        	return "reservaSucesso";
-    		}else
-    			FacesContext.getCurrentInstance().addMessage("data", new FacesMessage("Não existem projetores disponíveis nesta data"));
-    			return null;*/
-		} finally {
-			JPAUtil.finalizar();
-		}
-		return null;
-	}
 }
